4ff01f44625291f4f43080c65deebc7c73628649
Jerry Owusu <jerryowusu6133@gmail.com>
2022-09-06 13:02:35 +0000

    Update navbar with partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ get 'about-us', to: 'about#index', as: 'about'
+ root to: 'main#index'
 end
